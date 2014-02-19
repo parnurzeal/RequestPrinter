966cf95365559a7d9b6361d98e29b5030a7226c8
Theeraphol Wattanavekin <parnurzeal@gmail.com>
2014-02-19 15:36:03 +0900

    Remove and make it just simple

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-// Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+var http = require("http");
+
+http.createServer(function(request, response){
+      console.log(request);
+      response.writeHeader(200, {"Content-Type": "text/plain"});
+      response.write("Hello World!");
+      response.end();
+    }).listen(1337);
+
